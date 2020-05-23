590397985ab2b0a0928fc7b1c14eda6918dfedfb
andremarinho <andre.bmarinho@hotmail.com>
2020-05-23 06:08:43 -0300

    Correcao das rotas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,16 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { UsersComponent } from './views/users/users.component';
 
 
-const routes: Routes = [{
-  path: "" ,
-  component: HomeComponent
-},
-{
-  path: "/products" ,
-  component: ProductCrudComponent
-}];
+const routes: Routes = [
+{ path: '' ,  component: HomeComponent},
+{ path: 'users',   component: UsersComponent},
+{ path: 'products',component: ProductCrudComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
